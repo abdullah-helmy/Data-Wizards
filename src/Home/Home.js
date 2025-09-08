ef6d06067a93deab42f94f346ea18d2ec1377411
abdullah-helmy <460383468@qaliobia3.moe.edu.eg>
2025-09-08 20:43:55 +0300

    Adding the latest updates.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,23 @@
-const Home = () => {
+import { useEffect, useRef } from "react";
+
+const Home = (props) => {
+    const button = useRef(null);
+
+    useEffect(() => {
+        if (props.isDark) {
+            button.current.classList.add('bg-white', 'text-black');
+            button.current.classList.remove('bg-black', 'text-white');
+        } else {
+            button.current.classList.remove('bg-white', 'text-black');
+            button.current.classList.add('bg-black', 'text-white');
+        }
+    } ,[props.isDark]);
+
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">
-                Home Page
-            </h1>
+            <p className="pl-[200px] pt-[200px] font-bold text-3xl">The first end-to-end TEMPO support, <br />monitoring and forecasting air quality<br />in North America.</p>
+            <p className="pl-[200px] text-xl">Resources from NASA and other TEMPO institutions, real-time, <br />user-friendly air quality forecast application and tips to be<br />protected form pollution.</p>
+            <button ref={button} className={`ml-[200px] mt-[50px] px-4 py-3 border-none rounded-full text-lg`}>See latest forecasts</button>
         </div>
     );
 };
